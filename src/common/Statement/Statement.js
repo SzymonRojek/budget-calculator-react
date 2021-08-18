8945d57784221c8a23f495f38dab2f1c13544161
Szymon Rojek <sz.rojek@gmail.com>
2021-08-18 19:24:32 +0100

    edit Statement component - add props

--- src/common/Statement/Statement.js
+++ src/common/Statement/Statement.js
@@ -1,19 +1,21 @@
 import "./style.css";
 
-const Statement = ({ data }) => {
+const Statement = ({ data, removeItem }) => {
   return (
     <div>
       <ul className="list">
-        {data
-          ? data.map(({ id, amount, product, category }) => (
-              <li className="item" key={`item-${id}`}>
-                <span className="content">
-                  {id}. {amount}, {product} ({category})
-                </span>
-                <button className="button">X</button>
-              </li>
-            ))
-          : ""}
+        {data &&
+          data.map(({ id, amount, product, category }, index) => (
+            <li className="item" key={`item-${id}`}>
+              <span className="content">
+                {`${index + 1}. ${product}, (${category}),
+                ${amount}`}
+              </span>
+              <button className="button" onClick={() => removeItem(id)}>
+                X
+              </button>
+            </li>
+          ))}
       </ul>
     </div>
   );
