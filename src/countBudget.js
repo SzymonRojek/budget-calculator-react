f7782c6753ff63885cd0d49357ae46ebf2c6c199
Szymon Rojek <sz.rojek@gmail.com>
2021-08-18 19:20:59 +0100

    add function countBudget

---
+++ src/countBudget.js
@@ -0,0 +1,14 @@
+export const countBudget = (incomes, expenses) => {
+  let totalIncomes = 0;
+  let totalExpenses = 0;
+
+  for (const element of incomes) {
+    totalIncomes = totalIncomes + +element.amount;
+  }
+
+  for (const element of expenses) {
+    totalExpenses = totalExpenses + +element.amount;
+  }
+
+  return totalIncomes - totalExpenses;
+};
