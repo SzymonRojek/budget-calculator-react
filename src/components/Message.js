55b7b075061b5befdb355c71e5a656d7bc0dba27
Szymon Rojek <sz.rojek@gmail.com>
2021-08-19 13:25:15 +0100

    change Message footer background-color on submit

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,14 @@
 import "./style.css";
 
+const styledFooter = {
+  backgroundColor: "#66a182",
+  color: "white",
+};
 const Message = ({ isSubmitted }) => (
-  <footer className="submitted-footer">
+  <footer
+    className="submitted-footer"
+    style={isSubmitted ? styledFooter : null}
+  >
     {isSubmitted && <p className="animation-text-onSubmit">Submitted!</p>}
   </footer>
 );
