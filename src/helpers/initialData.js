deabbe62ca8cec8d2655795890df7c8f6d343e6d
Szymon Rojek <sz.rojek@gmail.com>
2021-08-19 09:37:59 +0100

    add initialData file

---
+++ src/helpers/initialData.js
@@ -0,0 +1,33 @@
+export const initialIncome = [
+  {
+    id: "1",
+    statement: "income",
+    product: "salary",
+    amount: "23525",
+    category: "work",
+  },
+  {
+    id: "2",
+    statement: "income",
+    product: "award",
+    amount: "600",
+    category: "food",
+  },
+];
+
+export const initialExpenses = [
+  {
+    id: "3",
+    statement: "expense",
+    product: "classical guitar",
+    amount: "3426",
+    category: "hobby",
+  },
+  {
+    id: "4",
+    statement: "expense",
+    product: "shoes",
+    amount: "2352352",
+    category: "sport",
+  },
+];
