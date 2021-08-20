f37ae58d472c69f4fb9d54acf8e1878389c491f8
Szymon Rojek <sz.rojek@gmail.com>
2021-08-20 23:57:54 +0100

    remove return from the Statement component

--- src/common/Statement/Statement.js
+++ src/common/Statement/Statement.js
@@ -1,23 +1,21 @@
 import "./style.css";
 
-const Statement = ({ data, removeItem, className }) => {
-  return (
-    <div className={className}>
-      <ul className="list">
-        {data &&
-          data.map(({ id, amount, product, category }) => (
-            <li className="item" key={`item-${id}`}>
-              <span className="content">
-                {product} ({category}), {amount} £
-              </span>
-              <button className="button" onClick={() => removeItem(id)}>
-                X
-              </button>
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
-};
+const Statement = ({ data, removeItem, className }) => (
+  <div className={className}>
+    <ul className="list">
+      {data &&
+        data.map(({ id, amount, product, category }) => (
+          <li className="item" key={`item-${id}`}>
+            <span className="content">
+              {product} ({category}), {amount} £
+            </span>
+            <button className="button" onClick={() => removeItem(id)}>
+              X
+            </button>
+          </li>
+        ))}
+    </ul>
+  </div>
+);
 
 export default Statement;
